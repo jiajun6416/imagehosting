4da442bede1e5c08ffa7bf93c66ee735c0a31c3a
jiajun <jiajun6416@gmail.com>
2017-09-07 00:53:24 +0800

    image vo

---
+++ src/main/java/com/jiajun/imagehosting/image/domain/imageEntity.java
@@ -0,0 +1,128 @@
+package com.jiajun.imagehosting.image.domain;
+
+import java.util.Date;
+
+public class imageEntity implements java.io.Serializable{
+
+	private static final long serialVersionUID = -6577799991386921190L;
+
+	private Integer id;
+
+    private Integer userId;
+
+    private String fileName;
+
+    private Integer width;
+
+    private Integer height;
+
+    private Integer size;
+
+    private Boolean isPublic;
+
+    private String httpUrl;
+
+    private Date createTime;
+
+    private Date deleteTime;
+
+    private Date uploadTime;
+
+    private Boolean isDelete;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Boolean getIsPublic() {
+        return isPublic;
+    }
+
+    public void setIsPublic(Boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    public String getHttpUrl() {
+        return httpUrl;
+    }
+
+    public void setHttpUrl(String httpUrl) {
+        this.httpUrl = httpUrl;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getDeleteTime() {
+        return deleteTime;
+    }
+
+    public void setDeleteTime(Date deleteTime) {
+        this.deleteTime = deleteTime;
+    }
+
+    public Date getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(Date uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public Boolean getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Boolean isDelete) {
+        this.isDelete = isDelete;
+    }
+}
