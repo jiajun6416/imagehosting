7a190d9663dfe9ec42dea9d6efb91bf10a8a467c
jiajun <jiajun6416@gmail.com>
2017-09-23 21:53:33 +0800

    自定义DispatcherServlet

---
+++ src/main/java/com/jiajun/imagehosting/web/CustomDispatcherServlet.java
@@ -0,0 +1,21 @@
+package com.jiajun.imagehosting.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 自定义 springmvc控制器
+ * Created by jiajun on 2017/09/23 18:47
+ */
+public class CustomDispatcherServlet extends org.springframework.web.servlet.DispatcherServlet{
+	private static final long serialVersionUID = 2709080075381025530L;
+	@Override
+	protected void noHandlerFound(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		if (pageNotFoundLogger.isWarnEnabled()) {
+			pageNotFoundLogger.warn("No mapping found for HTTP request with URI [" + request.getRequestURI() +
+					"] in DispatcherServlet with name '" + getServletName() + "'");
+		}
+		//跳转首页
+		request.getRequestDispatcher("/").forward(request, response);
+	}
+}	
